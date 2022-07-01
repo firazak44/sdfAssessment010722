477bb0f5e945e7f753f07d6e9a082b7f089127c0
Firdaus Razak <razakfirdaus95@gmail.com>
2022-07-01 14:25:36 +0800

    first com

---
+++ README.md
@@ -0,0 +1,1 @@
+#sdfAssessment010722
---
+++ sdfAssessment010722/src/main/java/sdfAsmtTasks/App.java
@@ -0,0 +1,13 @@
+package sdfAsmtTasks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
